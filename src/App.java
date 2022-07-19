65195ecbd27d2ae3dedf87ac8859006b421d52b7
Cristian Quintero <cristian.quinterom94@hotmail.com>
2022-07-19 11:49:24 -0500

    Se soluciona problema con el object[]

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+
+import java.util.ArrayList;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Corredor> carrera = new ArrayList<>();
+        carrera.add(new Corredor("Luis Gomez", "12", 1.68, 20, 22));
+        carrera.add(new Corredor("Juan Corredor", "28", 1.75, 24, 21));
+        carrera.add(new Corredor("Carlos Perez", "19", 1.71, 28, 24));
+        carrera.add(new Corredor("Maria Cadavid", "13", 1.80, 32, 22));
+        Object retorno[] = reporte(carrera);
+        System.out.println(Arrays.toString(retorno));
+
     }
 }
